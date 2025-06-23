a6dfbb42bf1f911ce3e98a10e7ba5344a848f6d2
Justinscreations <jlewis9103@gmail.com>
2025-06-23 11:20:11 -0400

    baller

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,3 +1,10 @@
+let projectImg1 = document.querySelector(".projectImg")
+let projectImg2 = document.querySelector(".project-img")
+
+
+
+
+
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
@@ -8,3 +15,22 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+
+projectImg1.addEventListener("mouseover", () => {
+  projectImg1.style.boxShadow = "5px 5px 50px #111";
+})
+
+projectImg1.addEventListener("mouseout", () => {
+  projectImg1.style.boxShadow = "none";
+
+})
+
+projectImg2.addEventListener("mouseover", () => {
+  projectImg2.style.boxShadow = "5px 5px 50px #111";
+})
+
+projectImg2.addEventListener("mouseout", () => {
+  projectImg2.style.boxShadow = "none";
+
+})
