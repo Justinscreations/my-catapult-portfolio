616dc009dc053c5b579ec3417485c0675eb50c2b
Justinscreations <jlewis9103@gmail.com>
2025-06-24 14:30:54 -0400

    um okay..

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let projectImg1 = document.querySelector(".projectImg");
 let projectImg2 = document.querySelector(".project-img");
-let contactImg1 = document.querySelector(".contact-img");
-let contactImg2 = document.querySelector(".contactImg");
+
 
 
 
@@ -36,20 +35,4 @@ projectImg2.addEventListener("mouseout", () => {
 
 })
 
-contactImg1.addEventListener("mouseover", () => {
-  contactImg1.style.boxShadow = "5px 5px 50px #111";
-})
 
-contactImg1.addEventListener("mouseout", () => {
-  contactImg1.style.boxShadow = "none";
-
-})
-
-contactImg2.addEventListener("mouseover", () => {
-  contactImg2.style.boxShadow = "5px 5px 5000px #111";
-})
-
-contactImg2.addEventListener("mouseout", () => {
-  contactImg2.style.boxShadow = "none";
-
-})
